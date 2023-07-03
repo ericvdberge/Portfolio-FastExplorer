9eb404a6263825b04af24e67603a17ecf28d09b2
eric.vandenberge@gmail.com <eric.vandenberge01@gmail.com>
2023-07-03 19:26:10 +0200

    fix error

--- server/api/folder.post.ts
+++ server/api/folder.post.ts
@@ -8,8 +8,8 @@ export default defineEventHandler(async (event: any) => {
 
 const readFiles = async (directory: string) => {
     return new Promise((resolve, reject) => {
-      let driveInfo = [] 
-      fs.readdir(directory, (err: Error, files: string[]) => {
+      let driveInfo: any = [] 
+      fs.readdir(directory, (err, files: string[]) => {
         if (err) {
           reject(err)
         }
@@ -21,9 +21,9 @@ const readFiles = async (directory: string) => {
                 path: file,
                 isDirectory: !file.includes('.')
               }
-            }
+            })
           })
+
         resolve(driveInfo)
-      });
     })
 }
